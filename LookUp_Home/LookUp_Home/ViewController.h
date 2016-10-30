fa59c69f237ee8420bbb755962a3cd3ab2b16f4b
jliu69 <liu_johnson@yahoo.com>
2016-10-30 12:02:04 -0400

    initial commit

---
+++ LookUp_Home/LookUp_Home/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LookUp_Home
+//
+//  Created by Johnson Liu on 10/30/16.
+//  Copyright © 2016 Home Office. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LookUp_Home/LookUp_Home/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LookUp_Home
+//
+//  Created by Johnson Liu on 10/30/16.
+//  Copyright © 2016 Home Office. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Home";
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
